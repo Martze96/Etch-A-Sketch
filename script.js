76e9d527865a5a39c48338aa2889006b3531ee60
Martin <maul_martin@gmx.de>
2021-09-03 16:08:55 +0200

    works so far

--- script.js
+++ script.js
@@ -1,36 +1,78 @@
 const container = document.querySelector("#container");
+const DEFAULT_COLOR = "red";
+let DEFAULT_SIZE = 16;
+let currentColor = "red";
 
 
-//creating div pixels 16 x 16
-for (let i = 1; i <= 256; i++) {
-    pixel = document.createElement("div");
-    
-    pixel.classList.add("pixel");
-    pixel.setAttribute("id",i);
-    pixel.style.backgroundColor = "white";
-    container.appendChild(pixel);
+const clearButton = document.getElementById("clearButton");
+let pixelSlider = document.querySelector("#pixelSizeControl");
 
+// On Start
+createPixels(DEFAULT_SIZE);
+changeSketchColor(DEFAULT_COLOR);
+
+//Update Label if Slider changed
+pixelSlider.onmousemove = function()  {
+    //update Label
+    document.getElementById("sliderLabel").innerHTML = pixelSlider.value + ' x ' + pixelSlider.value;
 }
-//Add EventListener if mousover then color pixel
-let pixelList = Array.from(container.childNodes);
-
-pixelList.forEach(function(item) {
-    item.addEventListener('mouseover', function() {
-        item.style.backgroundColor = "red";
-    });
-});
+// Reload if slider changed
+pixelSlider.oninput = function()  {
+        //clearPixels();
+        //update Sketchboard (create Pixels)
+        //createPixels(pixelSlider.value);
+        reloadPixels(pixelSlider.value);
+}
 
 //Add Eventlistener to Clear Button to make all Pixels white again
-let clearButton = document.getElementById("clearButton");
-clearButton.addEventListener("click",clearPixels);
+
+clearButton.addEventListener("click",function() {
+    reloadPixels(pixelSlider.value);
+});
+
+
 
 //function to clear pixels
 function clearPixels() {
-    let pixels = document.querySelectorAll(".pixel")
-    pixels.forEach(e => e.style.backgroundColor = "white");
+    container.innerHTML = "";
 }
 
-let pixelSlider = document.getElementById("pixelSizeControl");
-pixelSlider.oninput = function(){
-document.getElementById("sliderLabel").innerHTML = pixelSlider.value + ' x ' + pixelSlider.value;
+//function to create pixels
+function createPixels(size) {
+    //build Grid
+    container.style.gridTemplateColumns = 'repeat(' + size + ', 1fr)';
+    container.style.gridTemplateRows = 'repeat(' + size + ', 1fr)';
+    //place Pixels
+    for (let i = 1; i <= size*size; i++) {
+        const pixel = document.createElement("div");
+        //pixel.addEventListener('mousover',changeSketchColor("red"));
+        container.appendChild(pixel);
+      }
+      changeSketchColor(currentColor);
 }
+
+function changeColor(newColor) {
+    currentColor = newColor;
+}
+
+//function to Add EventListener if mousover with input Color
+function changeSketchColor(color){
+    
+    let pixelList = Array.from(container.childNodes);
+    pixelList.forEach(function(item) {
+        item.addEventListener('mouseover', function() {
+            item.style.backgroundColor = color;
+        });
+    });
+   
+}
+
+
+
+function reloadPixels(size) {
+    clearPixels();
+    createPixels(size);
+}
+
+
+

cd09d4fa0017abf4249ecb34e4213ab05006628c
Martin <maul_martin@gmx.de>
2021-09-03 13:13:38 +0200

    slider optic

--- script.js
+++ script.js
@@ -4,17 +4,33 @@ const container = document.querySelector("#container");
 //creating div pixels 16 x 16
 for (let i = 1; i <= 256; i++) {
     pixel = document.createElement("div");
-    pixel.style.border = "1px solid black";
     
     pixel.classList.add("pixel");
     pixel.setAttribute("id",i);
+    pixel.style.backgroundColor = "white";
     container.appendChild(pixel);
 
 }
+//Add EventListener if mousover then color pixel
 let pixelList = Array.from(container.childNodes);
-console.log(pixelList);
+
 pixelList.forEach(function(item) {
     item.addEventListener('mouseover', function() {
         item.style.backgroundColor = "red";
     });
 });
+
+//Add Eventlistener to Clear Button to make all Pixels white again
+let clearButton = document.getElementById("clearButton");
+clearButton.addEventListener("click",clearPixels);
+
+//function to clear pixels
+function clearPixels() {
+    let pixels = document.querySelectorAll(".pixel")
+    pixels.forEach(e => e.style.backgroundColor = "white");
+}
+
+let pixelSlider = document.getElementById("pixelSizeControl");
+pixelSlider.oninput = function(){
+document.getElementById("sliderLabel").innerHTML = pixelSlider.value + ' x ' + pixelSlider.value;
+}
